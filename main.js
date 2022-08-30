9fdbdeb63ca8e9f1e86be8d1b3adfe6f001d86ba
NachoLaprida <ignacio.laprida.2@gmail.com>
2022-08-30 20:05:38 -0300

    falta post id productos y su delete

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const express = require('express')
-const Contenedor = require("./contenedor");
-const Comentario = require("./contenedor");
+const {Contenedor, Comentario, Carrito} = require("./contenedor");
 const {Router} = express
 const routerProductos = Router()
+const routerCarrito = Router()
 const routerProductosRandom = Router()
 const {Server: HttpServer} = require('http')
 const {Server: IOServer} = require('socket.io');
-const { ok } = require('assert');
 const PORT = process.env.PORT || 8080
 
-const contenedor = new Contenedor('./prueba.txt')
+const contenedor = new Contenedor('./ecommerce.txt')
 const comentario = new Comentario('./comentarios.txt')
+const carrito = new Carrito('./carrito.txt')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -25,21 +25,7 @@ const io = new IOServer(httpServer)
 let messages = []
 
 io.on('connection', async (socket) => {
-    /*const comment = await comentario.getAll()
-    socket.emit('mensaje-servidor', {
-        m: "ok",
-        comentarios: comment
-    })
-    console.log(comment)
-    socket.on('mensaje-nuevo', async (req, res) => {
-        await comment.save(req.body) 
-        const comment = {
-            m:"comentario agregado",
-            comentario: comment
-        }
-    })
-    io.sockets.emit('mensaje-servidor', comment) */
-
+    /////// chat ////////////////
     const mensaje = {
         m: 'ok',
         messages
@@ -56,17 +42,166 @@ io.on('connection', async (socket) => {
         }
         io.sockets.emit('mensaje-servidor', mensaje)
     })
-    //console.log(mensaje)
+    //////// catalogo //////////////
 
     socket.on('mensaje-nuevo-producto', async (data) => {
         await contenedor.save(data)
         io.sockets.emit('mensaje-servidor-nuevo-producto', data)
     })
+
+    /////// email ///////////////
     socket.on('mensaje-email', (data) => {
         console.log('el email es',data)
     })
 
-}) 
+}) |
+
+
+
+
+/////////////////////////////    EJS        ////////////////////////
+//configuracion del ejs
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+
+routerProductos.get('', async (req, res) => {
+    const producto = await contenedor.getAll();
+    res.render('pages/index.ejs', { 
+        listaProductos: producto
+    })
+})
+
+routerProductos.post('/nuevoProducto', async (req, res) => {
+    await contenedor.save(req.body)
+    const ultimoProducto = await contenedor.getLastId()
+    res.render('pages/nuevoProducto.ejs', {
+        nuevoProducto: ultimoProducto   
+    })
+})
+
+///////////////// Productos ////////////
+
+routerProductos.get('/', async (req,  res) => {
+    try{
+        const allProducts = await contenedor.getAll() 
+        res.json(allProducts)
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+routerProductos.get('/:id', async (req, res)=> {
+    const idReq = req.params.id
+    console.log('idReq', )
+    const productoId = await contenedor.getById(+idReq); //consultar que hace el +
+    res.json(productoId);
+})
+
+
+routerProductos.post('/', async (req, res) => {
+    //console.log('el req', req.body)
+    const {name, price, thumbnail} = req.body
+    let newProduct = {name, price, thumbnail}
+    await contenedor.save(newProduct) 
+    res.json({
+        producto: newProduct 
+    })
+})
+routerProductos.put('/:id', async (req, res, ) => {
+    const { id } = req.params
+    const  {name, price, thumbnail} = req.body
+    let updateProduct = {name, price, thumbnail}
+    await contenedor.updateById({id: parseInt(id), ...updateProduct})
+    res.json({
+        producto: updateProduct
+    })       
+})
+
+routerProductos.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    await contenedor.delete(parseInt(id))
+    res.json({
+        msg: 'se borro el producto' 
+    })  
+})
+
+////////////// Carrito ////////////////////////////
+
+
+
+
+
+routerCarrito.post('/', async (req, res) => {
+    let newCarrito = await carrito.createCart()
+    res.json({
+        carrito: newCarrito 
+    })
+})
+routerCarrito.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    await carrito.delete(parseInt(id))
+    res.json({
+        msg: 'se borro el producto del carrito' 
+    })  
+})
+
+routerCarrito.get('/:id/productos', async (req,  res) => {
+    try{
+        const { id } = req.params
+        const cart = await carrito.getById(id)
+        res.json(cart.products)
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+/* routerCarrito.post('/:id/productos', async (req, res) => {
+    const cart = await carrito.getById(id)
+    cart.products.push()
+    console.log(cart)
+    res.json({
+        
+    })
+}) */
+
+////////// Routes
+app.use('/api/productos', routerProductos) 
+app.use('/api/carrito', routerCarrito) 
+
+
+
+
+
+
+
+
+const server = httpServer.listen(PORT, () => {
+    console.log(`Escuchando el puerto ${server.address().port}`)
+})
+
+server.on('error', (err) => {
+    console.log(err)
+})
+
+
+/* httpServer.listen(PORT, (err) => {
+    if(err) throw Error(`Error on server listen: ${err}`)
+    console.log(`Server is running on port: ${PORT}`)
+}) */
+
+//////////// productosRandom /////////
+
+/* routerProductosRandom.get('/', async (req, res) => {
+    const cantidad = await contenedor.getLenght();
+    const random =  Math.floor(Math.random() * cantidad ) + 1 ;
+    const productRandom = await contenedor.getById(random);
+    res.json(productRandom);
+})
+
+app.use('/api/productosRandom', routerProductosRandom)  */
 
 //////////////////////////////////////////////
 
@@ -142,112 +277,4 @@ routerProductos.post('/nuevoProducto', async (req, res) => {
     res.render('pages/nuevoProducto.pug', {
         nuevoProducto: ultimoProducto
     })
-}) */
-
-
-/////////////////////////////    EJS        ////////////////////////
-//configuracion del ejs
-app.set('view engine', 'ejs')
-app.set('views', './views')
-
-
-routerProductos.get('', async (req, res) => {
-    const producto = await contenedor.getAll();
-    res.render('pages/index.ejs', { 
-        listaProductos: producto
-    })
-})
-
-routerProductos.post('/nuevoProducto', async (req, res) => {
-    await contenedor.save(req.body)
-    const ultimoProducto = await contenedor.getLastId()
-    res.render('pages/nuevoProducto.ejs', {
-        nuevoProducto: ultimoProducto   
-    })
-})
-
-
-//////////////////////////////////////////////////////////////////
-
-/* app.get('/', (req, res) => {
-    res.json('Buenos dias')
-}) */
-
-///////////////// productos ////////////
-
-routerProductos.get('/', async (req,  res) => {
-    try{
-        const allProducts = await contenedor.getAll() 
-        res.json(allProducts)
-    }
-    catch(err){
-        console.log(err)
-    }
-})
-
-routerProductos.get('/:id', async (req, res)=> {
-    const idReq = req.params.id
-    //console.log('idReq', idReq)
-    const produtoId = await contenedor.getById(+idReq); //consultar que hace el +
-    res.json(produtoId);
-})
-
-
-routerProductos.post('/', async (req, res) => {
-    //console.log('el req', req.body)
-    const {name, price, thumbnail} = req.body
-    let newProduct = {name, price, thumbnail}
-    await contenedor.save(newProduct) 
-    res.json({
-        producto: newProduct 
-    })
-})
-routerProductos.put('/:id', async (req, res, ) => {
-    const { id } = req.params
-    const  {name, price, thumbnail} = req.body
-    let updateProduct = {name, price, thumbnail}
-    await contenedor.updateById({id: parseInt(id), ...updateProduct})
-    res.json({
-        producto: updateProduct
-    })       
-})
-
-routerProductos.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    await contenedor.delete(parseInt(id))
-    res.json({
-        msg: 'se borro el producto' 
-    })  
-})
-
-app.use('/api/productos', routerProductos) 
-
-
-//////////// productosRandom /////////
-
-/* routerProductosRandom.get('/', async (req, res) => {
-    const cantidad = await contenedor.getLenght();
-    const random =  Math.floor(Math.random() * cantidad ) + 1 ;
-    const productRandom = await contenedor.getById(random);
-    res.json(productRandom);
-})
-
-app.use('/api/productosRandom', routerProductosRandom)  */
-
-
-
-
-
-const server = httpServer.listen(PORT, () => {
-    console.log(`Escuchando el puerto ${server.address().port}`)
-})
-
-server.on('error', (err) => {
-    console.log(err)
-})
-
-
-/* httpServer.listen(PORT, (err) => {
-    if(err) throw Error(`Error on server listen: ${err}`)
-    console.log(`Server is running on port: ${PORT}`)
 }) */
